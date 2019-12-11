8edf01b36cf6692018a958c5ff8f3d89f8e01013
Carlos <carlos.anguix@gmail.com>
2019-12-11 20:42:17 +0100

    Filtro por añoDesde, hasta, seccion, tamanyo

--- Fallas/sirviendo/cliente.js
+++ Fallas/sirviendo/cliente.js
@@ -6,12 +6,13 @@ let todasSecciones;
 
 function listar() {
 
+	// Accedemos a la API de monumentos falleros de datos abiertos de valencia
 	fetch('http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON')
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (json) {
-			fallasJSON = json["features"];
+			fallasJSON = json.features;
 			rellenarCamposFiltros();
 		})
 		.catch(function (error) {
@@ -20,8 +21,7 @@ function listar() {
 
 	function rellenarCamposFiltros() {
 
-		let selectSecciones = document.getElementById('seccion');
-
+		// Recogemos las secciones no repetidas y las metemos en un array
 		for (let i = 0; i < fallasJSON.length; i++) {
 
 			let seccion = fallasJSON[i].properties.seccion;
@@ -29,12 +29,15 @@ function listar() {
 			if (!todasSecciones.includes(seccion)) {
 
 				todasSecciones.push(fallasJSON[i].properties.seccion);
-
 			}
 		}
 
+		// Lo ordenamos
 		todasSecciones.sort();
 
+		let selectSecciones = document.getElementById('seccion');
+
+		// Las añadimos al DOM
 		for (let i = 0; i < todasSecciones.length; i++) {
 
 			let optionSeccion = document.createElement('option');
@@ -42,16 +45,65 @@ function listar() {
 			optionSeccion.innerText = todasSecciones[i];
 			selectSecciones.appendChild(optionSeccion);
 		}
-
-
-
 	}
+}
+
+function refrescarFallas() {
+
+	let select = document.getElementById('seccion');
+	
+	let seccion = select.options[select.selectedIndex].value;
+
+	let anyoDesde = document.getElementById('anyoDesde').value;
+	if (anyoDesde == "") {
+		anyoDesde = 0;
+	}
+	
+	let anyoHasta = document.getElementById('anyoHasta').value;
+	if (anyoHasta == "") {
+		anyoHasta = 9999;
+	}
+	
+	let tamanyo = "none";
+	if (document.getElementById('principal').checked) {
+		tamanyo = document.getElementById('principal').value;
+	} else if (document.getElementById('infantil').checked) {
+		tamanyo = document.getElementById('infantil').value;
+	}
+
+	mostrarFallasFiltradas(seccion, anyoDesde, anyoHasta, tamanyo);
+}
+
+function mostrarFallasFiltradas(seccion, anyoDesde, anyoHasta, tamanyo) {
+
+	console.log(seccion);
+	console.log(anyoDesde);
+	console.log(anyoHasta);
+	console.log(tamanyo);
+
+	let fallasFiltradas = fallasJSON.filter(falla => {
+		return (seccion == "Todas" || falla.properties.seccion == seccion)
+		&& anyoDesde <= falla.properties.anyo_fundacion
+		&& anyoHasta >= falla.properties.anyo_fundacion;
+	});
+
+	console.log(fallasFiltradas);
+
 }
 
 function init() {
 
+	// Inicializamos lo necesario
 	todasSecciones = [];
 	listar();
+
+	// Añadimos los eventos
+	// refrescarFallas(seccion, añoDesde, añoHasta)
+	document.getElementById('seccion').addEventListener('change', refrescarFallas);
+	document.getElementById('anyoDesde').addEventListener('focusout', refrescarFallas);
+	document.getElementById('anyoHasta').addEventListener('focusout', refrescarFallas);
+	document.getElementById('principal').addEventListener('click', refrescarFallas);
+	document.getElementById('infantil').addEventListener('click', refrescarFallas);
 
 }
 
