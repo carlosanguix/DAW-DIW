aeeedafab6467cb6a043ba6018045090de5015c0
Carlos <carlos.anguix@gmail.com>
2019-12-11 18:35:35 +0100

    Rellenando el select secciones

--- Fallas/sirviendo/cliente.js
+++ Fallas/sirviendo/cliente.js
@@ -1,18 +1,57 @@
+// Lista de fallasJSON
+let fallasJSON;
+
+// Todas las secciones
+let todasSecciones;
+
 function listar() {
 
-	fetch('http://mapas.valencia.es/lanzadera/opendata/res_aceite/JSON')
+	fetch('http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON')
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (json) {
-			// Aquí tenemos el json
+			fallasJSON = json["features"];
+			rellenarCamposFiltros();
 		})
 		.catch(function (error) {
 			console.log("error");
 		});
+
+	function rellenarCamposFiltros() {
+
+		let selectSecciones = document.getElementById('seccion');
+
+		for (let i = 0; i < fallasJSON.length; i++) {
+
+			let seccion = fallasJSON[i].properties.seccion;
+
+			if (!todasSecciones.includes(seccion)) {
+
+				todasSecciones.push(fallasJSON[i].properties.seccion);
+
+			}
+		}
+
+		todasSecciones.sort();
+
+		for (let i = 0; i < todasSecciones.length; i++) {
+
+			let optionSeccion = document.createElement('option');
+			optionSeccion.setAttribute('value', todasSecciones[i]);
+			optionSeccion.innerText = todasSecciones[i];
+			selectSecciones.appendChild(optionSeccion);
+		}
+
+
+
+	}
 }
 
 function init() {
+
+	todasSecciones = [];
+	listar();
 
 }
 
