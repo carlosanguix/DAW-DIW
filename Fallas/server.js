ff031598b58d299dd78c3149ed498fe0456f4da4
Carlos <carlos.anguix@gmail.com>
2019-12-18 13:34:13 +0100

    el body de req llega vacio

--- Fallas/server.js
+++ Fallas/server.js
@@ -1,3 +1,8 @@
+/*****************/
+/* Palabras clave*/
+/* -MONGO        */
+/*****************/
+
 // Importando librerias
 const express = require('express');
 const path = require('path');
@@ -15,8 +20,9 @@ const mongoose = require('mongoose');
 
 // MONGO Parseamos las peticiones
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }));
+
 
 // MONGO Parseamos lo jsons
 app.use(bodyParser.json());
@@ -25,22 +31,27 @@ app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
 
 // MONGO Conexión
-mongoose.connect(dbConfig.url,{
-    useNewUrlParser:true}).then(()=>{
-        console.log(" * BDD Mongo cargada");
-    }).catch(err => {
-        console.log(" Error en la conexión con la BBDD : ",err);
-        process.exit();
-    });
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log(" * BDD Mongo cargada");
+}).catch(err => {
+    console.log(" Error en la conexión con la BBDD : ", err);
+    process.exit();
+});
 
 // MONGO Vamos a definir un "punto de inicio"
-app.get('/api/',(req,res)=>{
-    res.json({"message":"API de MongoFallero"});
+app.get('/api/', (req, res) => {
+    res.json({ "message": "API de MongoFallero" });
 });
 
 // Sirviendo ficheros desde el servidor
-app.use(express.static(path.join(__dirname,'sirviendo')));
+app.use(express.static(path.join(__dirname, 'sirviendo')));
+
+// Require Puntuaciones routes
+require('./app/routes/puntuaciones.routes.js')(app);
+
 // Puerto de escucha
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log(" * Corriendo en el puerto 3000");
 });
