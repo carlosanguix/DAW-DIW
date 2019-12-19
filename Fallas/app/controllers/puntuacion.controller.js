2e0393ebba0bafc5a0117596bcfd480dae2e33a4
Carlos <carlos.anguix@gmail.com>
2019-12-19 11:09:18 +0100

    estructura básica de consultas (comentarios)

--- Fallas/app/controllers/puntuacion.controller.js
+++ Fallas/app/controllers/puntuacion.controller.js
@@ -1,40 +1,82 @@
 const Puntuacion = require('../models/puntuacion.model.js');
 
-// Obtener todas las puntuaciones
-exports.findAll = (req,res) => {
-    Puntuacion.find().then(puntuaciones=>{
+// Obtener todas las puntuaciones (no la utilizaremos)
+exports.findAll = (req, res) => {
+
+    Puntuacion.find().then(puntuaciones => {
         res.send(puntuaciones);
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).send({
-            message: err.message || " Algo fue mal mientras los capturabamos a todos"
+            message: err.message || "Error al recuperar todos los datos"
         });
     });
 };
 
+// Obtener una puntuación a partir de dos campos (diFalla, IP)
+exports.existe = (req, res) => {
+
+    console.info(req.params.idFalla);
+    console.info(req.params.ip);
+
+    // Buscamos dicha puntuación
+    Puntuacion.find({ "idFalla": req.params.idFalla, "ip": req.params.ip })
+        .then(puntuacion => {
+            // Devolvemos o la puntuación encontrada o un booleano
+            res.send(puntuacion._id);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Error al recuperar un dato a partir de dos campos"
+            });
+        });
+};
+
 // Crear y salvar
-exports.create = (req,res)=>{
+exports.create = (req, res) => {
 
     console.log(req.body);
 
     // Validamos el puntuacion
-    if (!req.body){
+    if (!req.body) {
         console.log(req.body);
         return res.status(400).send({
-           message:"puntuacion Vacio..." 
+            message: "La puntuación está vacía o faltan campos"
         });
     }
 
+    // Después de validar la puntuación debemos preguntarle a la BBDD si ya existe ese campo que queremos introducir
+
+
+
+    // En caso de que no exista, lo introducimos
+    // Guardamos la puntuación recibida
+    puntuacion.save().then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Error al crear una nueva puntuación"
+        });
+    });
+
+    // En caso de que si exista...
+    // ...Buscar esa coincidencia y modificarla en base a la id de la puntuación
+
+
+
+    // En cualquiera de los dos casos, debemos devolver la puntuación y modificarsela al cliente (modificar las estrellas)...
+    // ... Tal vez no es necesario porque el cliente la modifica antes de validarla (está bien).
+
+
+
+    /* En principio esto no lo necesitamos ya porque la puntuación recibida o es correcta o no la introducimos.
     const puntuacion = new Puntuacion({
         idFalla: req.body.idFalla || "idFallaVacio",
         ip: req.body.ip || "127.0.0.1",
         puntuacion: req.body.puntuacion || 42
-    })
+    })*/
+};
 
-    puntuacion.save().then(data =>{
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message|| "Something was wrong creating puntuacion"
-        });
-    });
+// Modificar una puntuación en base al id de puntuación
+exports.update = (req, res) => {
+
+
 };
